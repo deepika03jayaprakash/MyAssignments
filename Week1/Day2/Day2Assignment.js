c6a74af1e96d92029f319cb0feb018fdce6cc13a
deepika03jayaprakash <deepika03jayaprakash@gmail.com>
2026-09-02 16:36:42 +0530

    Add files via upload
    Day2Assignment

---
+++ Week1/Day2/Day2Assignment.js
@@ -0,0 +1,13 @@
+let userName = "Deepika"
+console.log(typeof userName);
+let organization = "Infosys"
+console.log(typeof organization);
+let contactNumber=1234567890
+console.log(typeof contactNumber);
+let knowsAutomation = true
+console.log(typeof knowsAutomation)
+let usesPlaywright
+console.log(typeof usesPlaywright);
+
+
+
---
+++ Week1/Day2/javaScriptBasics.js
@@ -0,0 +1,23 @@
+var firstName = "Deepika"
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName ="Infosys"
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber = 1234567890
+console.log(mobileNumber)
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
+
+
+
