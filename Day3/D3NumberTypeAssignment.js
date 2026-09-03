445558b6c400df941af7fcbe6288498d31560a24
Deepika Jayaprakash <deepika03jayaprakash@gmail.com>
2026-09-03 17:08:39 +0530

    Add files via upload
    W1D3  Class and Home Assignments

---
+++ Day3/D3NumberTypeAssignment.js
@@ -0,0 +1,21 @@
+function number(a)
+{
+//let a=0;
+if(a>0){
+    console.log(a," is greater than zero and its positive number");
+
+}else if (a<0)
+{
+console.log(a, " is less than zero and its negative number");
+
+}
+else
+{
+    console.log(a, " is Zero. Hence its neutral");
+    
+
+}
+    
+}
+
+number(-5);
---
+++ Day3/Day3ClassAssignment.js
@@ -0,0 +1,16 @@
+ const browserVersion = "chrome"
+ function getBrowserVersion(){
+    // var browserVersion
+    if(browserVersion=="chrome"){
+        // var browserVersion = "chrome" // Undefined
+        let browserVersion ="chrome" // Chrome
+    }
+    console.log("version number inside the function",browserVersion);
+    
+    
+ }
+//  console.log("version number outside the function",browserVersion);
+
+ getBrowserVersion()
+
+
