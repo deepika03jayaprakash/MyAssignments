d50861def65877db2999168183b75a5684a768f8
Deepika Jayaprakash <deepika03jayaprakash@gmail.com>
2026-09-08 17:21:07 +0530

    Update W2D1ClassAssignment.js

--- Week2/Day1/W2D1ClassAssignment.js
+++ Week2/Day1/W2D1ClassAssignment.js
@@ -1,10 +1,10 @@
-let num=[56,78,90,23,90,76,43,56];
+let num=[56,78,90,23,90,76,43,56]; //Declaring numbers in array
 
-for(let i=0; i<num.length;i++)
+for(let i=0; i<num.length;i++) //Using for loop to check the numbers one by one in the array
 {
-        for(let j=i+1; j<num.length;j++)
+        for(let j=i+1; j<num.length;j++) //Using nested loop to check the next numbers in the array
         {
-            if(num[i]===num[j])
+            if(num[i]===num[j]) //Using if to check the duplicates
             {
                 console.log("Duplicate",num[i])
             }
