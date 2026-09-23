ccf88065615cf55f4c5392dda7055452af010835
Deepika Jayaprakash <deepika03jayaprakash@gmail.com>
2026-09-23 20:11:12 +0530

    Update decatholon.spec.ts

--- Marathon1/decatholon.spec.ts
+++ Marathon1/decatholon.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from "@playwright/test"
 
 test('DecatholonAddtoCart', async ({ page }) => {
 
-    await page.goto('https://www.decathlon.in/')
-    await page.waitForLoadState('networkidle')
+    await page.goto('https://www.decathlon.in/')   //Load the url
+    await page.waitForLoadState('networkidle')  //
     let homePage = page.locator('[data-test-id="header-desktop:decathlon-logo"]')
     await expect(homePage).toBeVisible({ timeout: 10000 })
     let Search = page.locator('[data-test-id="search-input-desktop:container"]')
     await expect(Search).toBeEditable({ timeout: 10000 })
     await Search.fill("shoes")
     await Search.press('Enter')
-     await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle');
     let pageTitle = await page.title() // To get the Page title
     console.log("Current Search Page title is:", pageTitle)
 
@@ -55,17 +55,22 @@ test('DecatholonAddtoCart', async ({ page }) => {
     await expect(toastheading).toBeVisible({ timeout: 5000 });
     const message = await toastheading.textContent();
     console.log("Toast Message Captured:", message);
-    await page.locator('[aria-label="Close"]').click()
-    //await page.getByRole('button',{name:"Close"}).click({force:true})  // How to close the pop up?
+    let promopopupclosebtn = page.locator('[aria-label="Close"]')
+
+    if (await promopopupclosebtn.isVisible({ timeout: 3000 })) {
+        await promopopupclosebtn.click()
+        console.log("Promotional popup modal successfully intercepted and closed")
+    }
+
     await page.locator('[data-test-id="header-desktop:cart-icon"]').click()
     let Total = await page.locator('[data-test-id="order-summary-item-container"]').last().innerText()
     console.log(Total)
 
 })
 
-/*Not able to close the some promotional popup in decatholon page after add to cart. tried different way - used escape and force:true and locator but nothing working
+/*Not able to close the some promotional popup in decatholon page after add to cart. tried different way - used escape and force:true and playwright locator but nothing working
 1. Esc:await page.keyboard.press('Escape');
 2.click({ force: true })
-3. X playwright locator not worked. but css selector worked. 
+3. X playwright locator not worked. but css selector worked (await page.getByRole('button',{name:"Close"}).click({force:true}))
 4.Any otherway is there to close without locator for sudden promotional popup?
 */
